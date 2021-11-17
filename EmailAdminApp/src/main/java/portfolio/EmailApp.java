8bce4c0b590bf0b07e537c2dc79f5d16ccc784e9
Muhktar <muhktarabare@gmail.comgit config --global user.email git config --global user.email muhktarabare@gmail.com>
2021-11-17 00:33:13 -0500

    Adding new email for Kelly Richards.

--- EmailAdminApp/src/main/java/portfolio/EmailApp.java
+++ EmailAdminApp/src/main/java/portfolio/EmailApp.java
@@ -5,6 +5,8 @@ public class EmailApp {
         Email em1 = new Email("John","Smith");
         System.out.println(em1.showInfo());
 
+        Email em2 = new Email("Kelly", "Richards");
+        System.out.println(em2.showInfo());
     }
 
 }
